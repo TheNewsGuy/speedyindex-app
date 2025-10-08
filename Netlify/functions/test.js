15ea2742c302e9a81fe122b64af4e4429d143f67
TheNewsGuy <randy@38digitalmarket.com>
2025-10-08 14:23:43 -0400

    Update test.js

--- Netlify/functions/test.js
+++ Netlify/functions/test.js
@@ -1,1 +1,13 @@
-
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({ 
+      message: 'Test function works!',
+      time: new Date().toISOString()
+    })
+  };
+};
